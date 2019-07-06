8ba0a9e50adb30e3d91e29d157fded8990e99715
Josh Lear <joshrlear@gmail.com>
2019-07-06 12:17:57 -0700

    sql to create shopping list

--- src/practice.js
+++ src/practice.js
@@ -6,16 +6,55 @@ const knexInstance = knex({
   connection: process.env.DB_URL
 });
 
-console.log(process.env.DB_URL);
-console.log(knexInstance.select('product_id', 'name', 'price', 'category')
-.from('amazong_products')
-.where({ name: 'Point of view gun' }));
 console.log('connection successful');
 
-/* knexInstance
-    .select('product_id', 'name', 'price', 'category')
-    .from('amazong_products')
-    .where({ name: 'Point of view gun' })
-    .then(result => {
+/* const qry = knexInstance
+  .select('product_id', 'name', 'price', 'category')
+  .from('amazong_products')
+  .where({ name: 'Point of view gun' })
+  .first()
+  .toQuery()
+  .then(result => {
+    console.log(result)
+  })
+
+console.log(qry) */
+
+/* function searchByProduceName(searchTerm) {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .where('name', 'ILIKE', `%${searchTerm}%`)
+      .then(result => {
         console.log(result)
-    }) */
+      })
+  }
+  
+  searchByProduceName('holo') */
+
+/*   function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+      .select('product_id', 'name', 'price', 'category')
+      .from('amazong_products')
+      .limit(productsPerPage)
+      .offset(offset)
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+  paginateProducts(2) */
+
+  function getProductsWithImages() {
+    knexInstance
+      .select('product_id', 'name', 'price', 'category', 'image')
+      .from('amazong_products')
+      .whereNotNull('image')
+      .then(result => {
+        console.log(result)
+      })
+  }
+  
+  getProductsWithImages()
